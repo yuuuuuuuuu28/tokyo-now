802c52615684454878d75a5b4ace4294f3fda635
yuuuuuuuuu28 <as.awesome28@gmail.com>
2026-09-09 20:22:15 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+const CACHE='tokyo-now-v003';
+const ASSETS=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>{
+  const u=new URL(e.request.url);
+  if(u.hostname==='api.data.metro.tokyo.lg.jp') return; // APIは常にオンライン取得
+  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)));
+});
