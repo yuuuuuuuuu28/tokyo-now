a381f620b7127285f9e361d4ac9787a71a1d0d17
yuuuuuuuuu28 <as.awesome28@gmail.com>
2026-09-09 20:55:29 +0900

    Add files via upload

--- tokyo-now-pwa/sw.js
+++ tokyo-now-pwa/sw.js
@@ -1,4 +1,4 @@
-const CACHE='tokyo-now-v003';
+const CACHE='tokyo-now-v0031';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
